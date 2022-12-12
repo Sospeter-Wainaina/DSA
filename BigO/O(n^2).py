499c673c737d89709d064f60cdde25b1e156d065
sospeter-wainaina <sospetergathungu2000@gmail.com>
2022-12-12 13:55:39 +0300

    O(n^2)

--- BigO/O(n^2).py
+++ BigO/O(n^2).py
@@ -9,3 +9,19 @@ logAllPairsOfArray(boxes)
 
 #nested loops we `multiply` the two loops O(n^2)
 #Quadratic Time 
+
+#Rule 4: Drop Non Dominants
+
+def printAllNumbersThenAllPairSums(numbers):
+    print('these are the numbers:')
+    for number in numbers:
+        print(number)
+
+    print('and these are their sums:')
+    for firstNumber in numbers:
+        for secondNumber in numbers:
+            print(firstNumber + secondNumber)
+
+printAllNumbersThenAllPairSums([1, 2, 3, 4, 5])
+# we drop the non dominants
+# O(n + n^2) = O(n^2)
