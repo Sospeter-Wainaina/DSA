e5307159b980afc7a271e3cafcc8891eb00a4ddd
sospeter-wainaina <sospetergathungu2000@gmail.com>
2022-12-12 10:44:42 +0300

    Data Structures and Algorithms

--- BigO/nemo.py
+++ BigO/nemo.py
@@ -1,5 +1,9 @@
 # Big O notation
+import time
+
 nemo = ['nemo']
+everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill',
+            'bloat', 'nigel', 'squirt', 'darla', 'hank']
 
 
 def findNemo(array):
@@ -8,4 +12,4 @@ def findNemo(array):
             print('FOUND NEMO !')
 
 
-findNemo(nemo)
+findNemo(everyone)
