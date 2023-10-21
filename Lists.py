14cf9dd5c8f296f5e0cb78e465f8abb16c6cca74
sospeter-wainaina <sospetergathungu2000@gmail.com>
2023-10-21 11:09:35 +0300

    Python Lists

---
+++ Lists.py
@@ -0,0 +1,21 @@
+# Built in Datastructure used to store a collection of items
+# -They are ordered
+# -They are mutable
+# -Allow duplicates
+
+integers = [1, 2, 3, 4]
+print(integers)
+
+stringList = ['Milk', 'Cheese', 'Butter']
+print(stringList)
+
+# Lists can store different data types
+
+mixeList = [1, 'spam', 2.33]
+print(mixeList)
+
+nestedList = [1, 2, 4, 5, ['test', 'milky', 'way']]
+print(nestedList)
+
+empty = []
+print(empty)
