bde0739ce74b35229b692d4e393c0b208f39d2d1
sospeter-wainaina <sospetergathungu2000@gmail.com>
2023-10-28 13:01:57 +0300

    Slicing

---
+++ middle_function.py
@@ -0,0 +1,12 @@
+# Write a function called middle that takes a list and returns a new list that contains all but the first and last elements.
+
+# myList = [1, 2, 3, 4]
+# middle(myList)  # [2,3]
+def middle(lst):
+    lst.pop()
+    lst.remove(lst[0])
+    print(lst)
+
+
+myList = [1, 2, 3, 4]
+middle(myList)  # [2,3]
