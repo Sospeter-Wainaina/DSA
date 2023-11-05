ba17dd5052a9a692e6d90a7146a640aabf5a29ba
sospeter-wainaina <sospetergathungu2000@gmail.com>
2023-11-05 13:44:53 +0300

    Dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,10 @@
+# Creating dictionaries
+my_dict = dict()
+print(my_dict)
+my_dict2 = {}
+print(my_dict2)
+
+eng_sp = dict(one='uno', two='dos', three='tres')
+print(eng_sp)
+eng_sp2 = {'one': 'uno', 'two': 'dos', 'three': 'tres'}
+print(eng_sp2)
