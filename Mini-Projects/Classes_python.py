d2740718a7950e0fecd7d6cf900774efd91f0db2
sospeter-wainaina <sospetergathungu2000@gmail.com>
2023-12-12 10:22:05 +0300

    Classes with python

--- Mini-Projects/Classes_python.py
+++ Mini-Projects/Classes_python.py
@@ -8,23 +8,37 @@
 # Say we wanted to create a class called Employee that represents information about an employee.
 
 class Employee:
-    pass
+    def __init__(self, first, last, pay):
+        self.first = first #these are instance variables
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' +last + '@company.com'
 
-emp_1 = Employee()
-emp_2 = Employee()
+emp_1 = Employee('Sospeter','Wainana',120000)
+emp_2 = Employee('Ivy','Mwaniki',120000)
+
+# we can see that it was easier to create the instances of the class.
+print(emp_1.email)
+print(emp_2.email)
 
 # print(emp_1) #we can see that both of these instances were created at different locations in memory. #<__main__.Employee object at 0x00000256228419A0>
 # print(emp_2) #<__main__.Employee object at 0x0000025622841B20>
 
-emp_1.first = 'Sospeter'
-emp_1.last = 'Wainaina'
-emp_1.email = 'Sospeter.Wainaina@company.com'
-emp_1.pay = 120000
+# emp_1.first = 'Sospeter'
+# emp_1.last = 'Wainaina'
+# emp_1.email = 'Sospeter.Wainaina@company.com'
+# emp_1.pay = 120000
+#
+# emp_2.first = 'Test'
+# emp_2.last = 'User'
+# emp_2.email = 'Test.User@company.com'
+# emp_2.pay = 60000
+#
+# print(emp_1.email)
+# print(emp_2.email)
 
-emp_2.first = 'Test'
-emp_2.last = 'User'
-emp_2.email = 'Test.User@company.com'
-emp_2.pay = 60000
-
-print(emp_1.email)
-print(emp_2.email)
+# This is a lot of work to do for each employee.
+# We can instead use a special method called __init__ (dunder init). which is a constructor.
+#self is the instance of the class. It is used to access variables that belong to the class.
+# A constructor is a special method that is run automatically when an instance of a class is created.
+# It is used to initialize the object's state.
